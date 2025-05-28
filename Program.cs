8abed3d349024574ce407843ac071b6e081fbf8d
juyoung07 <yunju5090@gmail.com>
2025-05-28 10:09:51 +0900

    오버로딩 주의점

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace PropExample
     {
         static void Main(string[] args)
         {
+            TestOver(3);
         }
+
+        // 오버로딩
+        public static int TestOver(int input) {  return 0; }
+        public static int TestOver(double input) { return 0; }
+        //public static bool TestOver(int input) { return true; }  // 매개변수가 같아서 error
     }
 }
