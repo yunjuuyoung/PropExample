b970cd7c0b2d50a3fe7fa52730ff83704d54d49c
juyoung07 <yunju5090@gmail.com>
2025-06-02 13:43:54 +0900

    싱글톤 패턴 예시

--- Product.cs
+++ Product.cs
@@ -10,10 +10,15 @@ namespace PropExample
     {
         public string name;
         public int price;
+        private static Product product;
 
         // 팩토리 메서드 패턴
         public static Product getInstance(string name, int price)
         {
+            if (product == null)
+            {
+                product = new Product(name, price);
+            }
             return new Product(name, price);
         }
 
