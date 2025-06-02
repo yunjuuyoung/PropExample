83beabf967fccf2d70f727a5cc9b5c87fea04c7f
juyoung07 <yunju5090@gmail.com>
2025-06-02 14:57:44 +0900

    getter, setter 방식으로 구현

---
+++ Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    class Box
+    {
+        private int width;
+        private int height;
+
+        public Box(int width, int height)
+        {
+            if (width > 0 && height > 0)  // 원래는 예외처리 해야 함
+            {
+                this.width = width;
+                this.height = height;
+            } else
+            {
+                Console.WriteLine("너비와 높이는 자연수로 초기화해주세요!");
+            }
+        }
+
+        public int getWidth() { return width; }
+        public void setWidth(int width)
+        {
+            if (width > 0) // 원래는 예외처리 해야 함
+            { 
+                this.width = width; 
+            }   
+            else 
+            { 
+                Console.WriteLine("너비는 자연수로 초기화해주세요!");
+            }
+        }
+        public int getHeiht() { return height; }
+        public void setHeight(int height)
+        {
+            if (height > 0) // 원래는 예외처리 해야 함 
+            { 
+                this.height = height; 
+            }   
+            else 
+            { 
+                Console.WriteLine("너비는 자연수로 초기화해주세요!"); 
+            }
+        }
+        public int Area() { return width * height; }
+    }
+}
